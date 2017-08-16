7517d84d7fc4abf494a2f83ca574b04129b968f5
Hendrik <h.sommer@wwu.de>
2017-08-16 09:37:18 +0200

    variable

--- main.c
+++ main.c
@@ -11,25 +11,25 @@ char* printing_order = "vaow";
 int print_units = 1;
 
 //Two values have to be given to be able to calculate the rest
-int calc_values(mpf_t *volt, mpf_t *amp, mpf_t *res, mpf_t *powe){
+int calc_values(mpf_t *volt, mpf_t *amp, mpf_t *res, mpf_t *power){
     if( mpf_sgn(*volt) && mpf_sgn(*amp)){
         mpf_div(*res, *volt, *amp);
-        mpf_mul(*powe, *volt, *amp);
+        mpf_mul(*power, *volt, *amp);
     }
     else if( mpf_sgn(*volt) && mpf_sgn(*res)){
         mpf_div(*amp, *volt, *res);
-        mpf_mul(*powe, *volt, *amp);
+        mpf_mul(*power, *volt, *amp);
     }
-    else if( mpf_sgn(*volt) && mpf_sgn(*powe)){
-        mpf_div(*amp, *powe, *volt);
+    else if( mpf_sgn(*volt) && mpf_sgn(*power)){
+        mpf_div(*amp, *power, *volt);
         mpf_div(*res, *volt, *amp);
     }
     else if( mpf_sgn(*amp) && mpf_sgn(*res)){
         mpf_mul(*volt, *res, *amp);
-        mpf_mul(*powe, *volt, *amp);
+        mpf_mul(*power, *volt, *amp);
     }
-    else if( mpf_sgn(*amp) && mpf_sgn(*powe)){
-        mpf_div(*volt, *powe, *amp);
+    else if( mpf_sgn(*amp) && mpf_sgn(*power)){
+        mpf_div(*volt, *power, *amp);
         mpf_div(*res, *volt, *amp);
     }
     else{
@@ -40,7 +40,7 @@ int calc_values(mpf_t *volt, mpf_t *amp, mpf_t *res, mpf_t *powe){
 }
 
 //print all given values in a list, ending with newline
-int print_values(mpf_t *volt, mpf_t *amp, mpf_t *res, mpf_t *powe){
+int print_values(mpf_t *volt, mpf_t *amp, mpf_t *res, mpf_t *power){
 
     if(print_units){
         for(int i = 0; i < strlen(printing_order); i++){
@@ -49,7 +49,7 @@ int print_values(mpf_t *volt, mpf_t *amp, mpf_t *res, mpf_t *powe){
                 break;
                 case 'v' : gmp_printf ("% .*FfV", DISP_PREC, *volt, DISP_PREC);
                 break;
-                case 'w' : gmp_printf ("% .*FfW", DISP_PREC, *powe, DISP_PREC);
+                case 'w' : gmp_printf ("% .*FfW", DISP_PREC, *power, DISP_PREC);
                 break;
                 case 'o' : gmp_printf ("% .*FfO", DISP_PREC, *res, DISP_PREC);
                 break;  
@@ -63,7 +63,7 @@ int print_values(mpf_t *volt, mpf_t *amp, mpf_t *res, mpf_t *powe){
                 break;
                 case 'v' : gmp_printf ("% .*Ff", DISP_PREC, *volt, DISP_PREC);
                 break;
-                case 'w' : gmp_printf ("% .*Ff", DISP_PREC, *powe, DISP_PREC);
+                case 'w' : gmp_printf ("% .*Ff", DISP_PREC, *power, DISP_PREC);
                 break;
                 case 'o' : gmp_printf ("% .*Ff", DISP_PREC, *res, DISP_PREC);
                 break;  
@@ -76,12 +76,12 @@ int print_values(mpf_t *volt, mpf_t *amp, mpf_t *res, mpf_t *powe){
 
 
 //assign input values to the correct units
-int init_values(mpf_t *volt, mpf_t *amp, mpf_t *res, mpf_t *powe, char *first, char *sec){
+int init_values(mpf_t *volt, mpf_t *amp, mpf_t *res, mpf_t *power, char *first, char *sec){
 
     mpf_init(*volt);
     mpf_init(*amp);
     mpf_init(*res);
-    mpf_init(*powe);
+    mpf_init(*power);
 
     char first_unit = first[strlen(first) - 1];
     char second_unit = sec[strlen(sec) - 1];
@@ -102,9 +102,9 @@ int init_values(mpf_t *volt, mpf_t *amp, mpf_t *res, mpf_t *powe, char *first, c
         break; 
         case 'o' : mpf_set_str(*res, first, 10);
         break; 
-        case 'W' : mpf_set_str(*powe, first, 10);
+        case 'W' : mpf_set_str(*power, first, 10);
         break; 
-        case 'w' : mpf_set_str(*powe, first, 10);
+        case 'w' : mpf_set_str(*power, first, 10);
         break; 
         default : return 0;
              
@@ -123,9 +123,9 @@ int init_values(mpf_t *volt, mpf_t *amp, mpf_t *res, mpf_t *powe, char *first, c
         break; 
         case 'o' : mpf_set_str(*res, sec, 10);
         break; 
-        case 'W' : mpf_set_str(*powe, sec, 10);
+        case 'W' : mpf_set_str(*power, sec, 10);
         break; 
-        case 'w' : mpf_set_str(*powe, sec, 10);
+        case 'w' : mpf_set_str(*power, sec, 10);
         break; 
         default : return 0;
              
@@ -137,7 +137,7 @@ int init_values(mpf_t *volt, mpf_t *amp, mpf_t *res, mpf_t *powe, char *first, c
 
 int main(int argc, char **argv){
 
-    mpf_t i_volt, i_amp, i_res, i_powe;
+    mpf_t i_volt, i_amp, i_res, i_power;
     int c;
 
     opterr = 0;
@@ -165,9 +165,9 @@ int main(int argc, char **argv){
       }
 
 
-    if(init_values(&i_volt, &i_amp, &i_res, &i_powe, argv[optind], argv[optind + 1]))
-        if(calc_values(&i_volt, &i_amp, &i_res, &i_powe))
-            print_values(&i_volt, &i_amp, &i_res, &i_powe);
+    if(init_values(&i_volt, &i_amp, &i_res, &i_power, argv[optind], argv[optind + 1]))
+        if(calc_values(&i_volt, &i_amp, &i_res, &i_power))
+            print_values(&i_volt, &i_amp, &i_res, &i_power);
 
     
     return 1;
